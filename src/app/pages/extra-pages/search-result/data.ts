89e79c7b68fdffb990e83cb58683cd835a7c2fe6
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2025-11-04 16:39:35 -0500

    fix(cards): adjust indentation and formatting in card templates for consistency

--- src/app/pages/extra-pages/search-result/data.ts
+++ src/app/pages/extra-pages/search-result/data.ts
@@ -1,88 +1,89 @@
-import { AllResult, UserResult } from "./search-result.model";
+import { AllResult, UserResult } from './search-result.model';
 
-const USERS: UserResult[] = [{
+const USERS: UserResult[] = [
+  {
     id: 1,
-    userName: "Chadengle",
-    email: "mediaheader@mail.com",
-    avatar: "/assets/images/users/user-1.jpg",
-    bio: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
-},
-{
+    userName: 'Chadengle',
+    email: 'mediaheader@mail.com',
+    avatar: './assets/images/users/user-1.jpg',
+    bio: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.',
+  },
+  {
     id: 2,
-    userName: "Halette Boivin",
-    email: "mediaheader@mail.com",
-    avatar: "/assets/images/users/user-2.jpg",
-    bio: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
-},
-{
+    userName: 'Halette Boivin',
+    email: 'mediaheader@mail.com',
+    avatar: './assets/images/users/user-2.jpg',
+    bio: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.',
+  },
+  {
     id: 3,
-    userName: "Lucas Sabourin",
-    email: "mediaheader@mail.com",
-    avatar: "/assets/images/users/user-3.jpg",
-    bio: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
-},
-{
+    userName: 'Lucas Sabourin',
+    email: 'mediaheader@mail.com',
+    avatar: './assets/images/users/user-3.jpg',
+    bio: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.',
+  },
+  {
     id: 4,
-    userName: "Donatien Brunelle",
-    email: "mediaheader@mail.com",
-    avatar: "/assets/images/users/user-4.jpg",
-    bio: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
-},
-{
+    userName: 'Donatien Brunelle',
+    email: 'mediaheader@mail.com',
+    avatar: './assets/images/users/user-4.jpg',
+    bio: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.',
+  },
+  {
     id: 5,
-    userName: "Karel Auberjo",
-    email: "mediaheader@mail.com",
-    avatar: "/assets/images/users/user-5.jpg",
-    bio: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
-}
+    userName: 'Karel Auberjo',
+    email: 'mediaheader@mail.com',
+    avatar: './assets/images/users/user-5.jpg',
+    bio: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.',
+  },
 ];
 
 const ALLRESULTS: AllResult[] = [
-    {
-        id: 1,
-        title: 'Zircos - Responsive Admin Template',
-        url: 'http://coderthemes.com/zircos/index.html',
-        content: `<p class="mb-0 text-muted">Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>`
-    },
-    {
-        id: 2,
-        title: 'Uplon - Responsive Bootstrap 4 Web App Kit',
-        url: 'http://themeforest.net/user/coderthemes/portfolio?ref=coderthemes',
-        content: `<p class="mb-0 text-muted">Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>`,
-    },
-    {
-        id: 3,
-        content: USERS[0]
-    },
-    {
-        id: 4,
-        title: 'Zircos - Responsive Admin Template',
-        url: 'http://ubold.coderthemes.com/',
-        content: `<p class="mb-0 text-muted">Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>`
-    },
-    {
-        id: 5,
-        title: 'Uplon - Responsive Bootstrap 4 Web App Kit',
-        url: 'http://themeforest.net/user/coderthemes/portfolio?ref=coderthemes',
-        content: `
+  {
+    id: 1,
+    title: 'Zircos - Responsive Admin Template',
+    url: 'http://coderthemes.com/zircos/index.html',
+    content: `<p class="mb-0 text-muted">Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>`,
+  },
+  {
+    id: 2,
+    title: 'Uplon - Responsive Bootstrap 4 Web App Kit',
+    url: 'http://themeforest.net/user/coderthemes/portfolio?ref=coderthemes',
+    content: `<p class="mb-0 text-muted">Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>`,
+  },
+  {
+    id: 3,
+    content: USERS[0],
+  },
+  {
+    id: 4,
+    title: 'Zircos - Responsive Admin Template',
+    url: 'http://ubold.coderthemes.com/',
+    content: `<p class="mb-0 text-muted">Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>`,
+  },
+  {
+    id: 5,
+    title: 'Uplon - Responsive Bootstrap 4 Web App Kit',
+    url: 'http://themeforest.net/user/coderthemes/portfolio?ref=coderthemes',
+    content: `
         <div class="">
         <img src="../assets/images/small/img-1.jpg" height="48" alt="image"/>
         <img src="../assets/images/small/img-2.jpg" height="48" alt="image"/>
         <img src="../assets/images/small/img-3.jpg" height="48" alt="image"/>
         <img src="../assets/images/small/img-4.jpg" height="48" alt="image"/>
         </div>`,
-    },
-    {
-        id: 6,
-        title: 'Zircos - Responsive Admin Template',
-        url: 'http://ubold.coderthemes.com/',
-        content: `<p class="mb-0 text-muted">Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>`
-    },
-    {
-        id: 7,
-        title: 'Uplon - Responsive Bootstrap 4 Web App Kit',
-        url: 'http://themeforest.net/user/coderthemes/portfolio?ref=coderthemes',
-        content: `
+  },
+  {
+    id: 6,
+    title: 'Zircos - Responsive Admin Template',
+    url: 'http://ubold.coderthemes.com/',
+    content: `<p class="mb-0 text-muted">Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>`,
+  },
+  {
+    id: 7,
+    title: 'Uplon - Responsive Bootstrap 4 Web App Kit',
+    url: 'http://themeforest.net/user/coderthemes/portfolio?ref=coderthemes',
+    content: `
         <p class="mb-0 text-muted">
         Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.
         </p>
@@ -93,9 +94,7 @@ const ALLRESULTS: AllResult[] = [
         </div>
         </div>
         </div>`,
-    }
+  },
 ];
-
-
 
 export { ALLRESULTS, USERS };
--- src/app/transloco.config.ts
+++ src/app/transloco.config.ts
@@ -11,7 +11,7 @@ export class TranslocoHttpLoaderService implements TranslocoLoader {
   private http = inject(HttpClient);
 
   getTranslation(lang: string): Observable<Translation> {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(`./assets/i18n/${lang}.json`);
   }
 }
 
