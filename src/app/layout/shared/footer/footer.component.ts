2afb2f5c97c30c3d18e35d6c72f99ba644e1dd3d
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2025-12-18 16:52:34 -0500

    feat: add internationalization support for footer links in English and Spanish

--- src/app/layout/shared/footer/footer.component.ts
+++ src/app/layout/shared/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  imports: [CommonModule, DatePipe],
+  imports: [CommonModule, DatePipe, TranslocoModule],
   standalone: true,
 })
 export class FooterComponent implements OnInit {
